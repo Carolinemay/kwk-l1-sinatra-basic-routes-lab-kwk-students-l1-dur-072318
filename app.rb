c1f2590f3de4939603fdb0cf75bbbddb0fa5a25f
Carolinemay <czhou@students.chccs.k12.nc.us>
2018-07-31 17:14:50 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "welcome to my life"
+  end
+  
+  get '/name' do
+    "my name is Caroline"
+  end
+  get '/hometown' do
+    "my hometown is iowa city"
+  end
+  get '/favorite song' do
+   "my favorite song is thinking bout you"
+ end
 end
